c8eea376df347cd220590b7055552703814a9e63
Callum Cunningham <ccunningham@redev.co.uk>
2024-10-18 12:13:48 +0100

    Add migration for WorkerNumber.

---
+++ MultiManagementSystem/Migrations/20241018111306_WorkerNumberMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkerNumberMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeType",
+                table: "EmployedWorkers",
+                newName: "EmployeeType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeType",
+                table: "EmployedWorkers",
+                newName: "EmployeType");
+        }
+    }
+}
