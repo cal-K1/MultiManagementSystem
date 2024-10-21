026aae297fd227d8e598e135f3ca265b8d40fffd
Callum Cunningham <ccunningham@redev.co.uk>
2024-10-21 11:17:24 +0100

    Added pages to NavMenu and improved home Page by adding buttons to pages.

---
+++ MultiManagementSystem/Components/Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MultiManagementSystem.Components.Pages;
+
+public partial class Home
+{
+    [Inject]
+    NavigationManager NavigationManager { get; set; } = default!;
+
+    private void NavigateApply()
+    {
+        NavigationManager.NavigateTo("/apply");
+    }
+
+    private void NavigateView()
+    {
+        NavigationManager.NavigateTo("/view");
+    }
+
+    private void NavigateCreate()
+    {
+        NavigationManager.NavigateTo("/create");
+    }
+
+    private void NavigateRequest()
+    {
+        NavigationManager.NavigateTo("/request");
+    }
+}
