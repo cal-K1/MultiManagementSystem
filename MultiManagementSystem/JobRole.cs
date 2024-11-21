c37fff78822839f397e62706d4f75d192fb5e5fe
Callum Cunningham <ccunningham@redev.co.uk>
2024-11-21 12:59:56 +0000

    Rename Id.

--- MultiManagementSystem/JobRole.cs
+++ MultiManagementSystem/JobRole.cs
@@ -2,7 +2,7 @@
 
 public class JobRole
 {
-    public string Id { get; set; } = string.Empty;
+    public string JobRoleId { get; set; } = string.Empty;
     public string JobTitle { get; set; } = string.Empty;
     public int Salary { get; set; } = 0;
     public string Description { get; set; } = string.Empty;
