625bd1c9a74eaf451c098fec8a46118504ce7504
Callum Cunningham <ccunningham@redev.co.uk>
2024-11-21 10:31:29 +0000

    Added db migrations.

---
+++ MultiManagementSystem/Migrations/20241121102938_WorkerPropertyChangeAddWorkerCountry.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkerPropertyChangeAddWorkerCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Country",
+                table: "Workers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Workers");
+        }
+    }
+}
