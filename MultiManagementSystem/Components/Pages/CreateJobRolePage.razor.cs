2a7dafba00e29ce8ff37d1edddb33709f375e51c
Callum Cunningham <ccunningham@redev.co.uk>
2024-12-02 16:15:18 +0000

    Added outline for page.

---
+++ MultiManagementSystem/Components/Pages/CreateJobRolePage.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiManagementSystem.Components.Pages;
+
+public partial class CreateJobRolePage
+{
+    public JobRole NewJobRole { get; set; } = default!;
+    public string JobTitle { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Salary { get; set; } = 0;
+
+    public void SetValues()
+    {
+
+    }
+}
