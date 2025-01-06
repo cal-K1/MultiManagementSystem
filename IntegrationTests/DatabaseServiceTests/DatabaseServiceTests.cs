17266ae22a09356f77323938f859ce4c5f70d02e
Callum Cunningham <ccunningham@redev.co.uk>
2025-01-06 09:53:40 +0000

    Fixed nuget.

--- IntegrationTests/DatabaseServiceTests/DatabaseServiceTests.cs
+++ IntegrationTests/DatabaseServiceTests/DatabaseServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
 using MultiManagementSystem.Data;
 using MultiManagementSystem.Models;
